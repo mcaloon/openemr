2e5f15f68b865745fb204eb08938944d04a9464c
mac <mcaloon@partnershealthcareanalytics.com>
2012-05-16 16:25:19 -0400

    added new formatted dx_code and sg_code for ICD9 decimal points

--- sql/create_ICD9.sql
+++ sql/create_ICD9.sql
@@ -4,8 +4,9 @@
 DROP TABLE IF EXISTS `icd9_dx_code`;
 CREATE TABLE `icd9_dx_code` (
   `dx_code`		varchar(5),	-- ICD9-DX code. 
-  `short_desc`		varchar(60),	-- Short description
-  `long_desc`		varchar(300)	-- Long description
+  `formatted_dx_code`   varchar(6),     -- ICD9-DX code with decimal positions.
+  `short_desc`          varchar(60),    -- Short description
+  `long_desc`           varchar(300)    -- Long description
 ) ENGINE=MyISAM;
 
 --
@@ -13,7 +14,8 @@ CREATE TABLE `icd9_dx_code` (
 --
 DROP TABLE IF EXISTS `icd9_sg_code`;
 CREATE TABLE `icd9_sg_code` (
-  `sg_code`		varchar(5),	-- ICD9-SG code
+  `sg_code`             varchar(5),     -- ICD9-SG code
+  `formatted_sg_code`   varchar(6),     -- ICD9-SG code with decimal positions.
   `short_desc`		varchar(60),	-- Short description
   `long_desc`		varchar(300)	-- Long description
 ) ENGINE=MyISAM;
